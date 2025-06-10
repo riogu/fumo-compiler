c5c1edd45f03788d18676dc9f485d4fc40dfc356
riogu <egas.g.ribeiro@gmail.com>
2025-06-10 12:23:57 +0100

    small changes

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -7,6 +7,8 @@
 #define add_token(tkn) tokens.push_back(Token {.type = TokenType::tkn, add_token_info})
 #define add_and_consume_token(tkn) do {tokens.push_back(Token {.type = TokenType::tkn, add_token_info}); get_curr();} while(0)
 #define next_is(tkn) (file_stream.peek() == tkn)
+#define make_token(tkn) Token {.type = TokenType::tkn, add_token_info}
+#define make_and_consume_token(tkn) ({get_curr(); Token {.type = TokenType::tkn, add_token_info};})
 
 [[nodiscard]] Result<Vec<Token>, Str> Lexer::tokenize_file(const fs::path _file_name) {
     __FUMO_FILE__ = _file_name.filename(); __FUMO_LINE__ = peek_line();
@@ -27,8 +29,7 @@
             continue;
         }
         switch (curr) {
-            // FIXME: 
-            //        - fix issues with EOF during the lexer
+            // FIXME: - fix issues with EOF during the lexe
             //        - add -> punctuator case
             //
             // -----------------------------------------------------------
@@ -38,12 +39,12 @@
             cases(has_double_and_equals);
             cases(ignore);
             // -----------------------------------------------------------
-            // triple cases
+            // triple cases are handled individually
             case '.':
                 if ((file_stream.peek() == '.')) {
                     if (get_curr(); (file_stream.peek() == '.')) add_and_consume_token(dot_dot_dot);
                     else
-                        PANIC(fmt_error("Expected expression."));
+                        PANIC(fmt_error("Expected complete '...' ellipsis."));
                 } else 
                       add_token(dot);
                 break;
@@ -70,24 +71,21 @@
                 else // <
                     add_token(greater);
                 break;
-                // clang-format off
             // -----------------------------------------------------------
             // special cases
             case '/':
                 if (next_is('/'))
                     while (!next_is(EOF) && !next_is('\n')) curr = get_curr();
                 else
-                    tokens.push_back(next_is('=')
-                                     ? ({get_curr(); Token {.type = TokenType::division_equals, add_token_info};})
-                                     : Token {.type = TokenType::division, add_token_info});
+                    tokens.push_back(next_is('=') ? make_and_consume_token(division_equals)
+                                                  : make_token(division));
                 break;
 
             case '\n': __FUMO_LINE_NUM__++; __FUMO_LINE_OFFSET__ = 0; __FUMO_LINE__ = peek_line(); break;
 
             case '#':
-                tokens.push_back(next_is('#')
-                                 ? ({get_curr();Token {.type = TokenType::hashtag_hashtag, add_token_info};})
-                                 : Token {.type = TokenType::hashtag, add_token_info});
+                tokens.push_back(next_is('#') ? make_and_consume_token(hashtag_hashtag)
+                                              : make_token(hashtag));
                 break;
 
             default: PANIC(fmt_error("Source file is not valid ASCII."));
