84f16f445c07d43fc12b91a3d98771f419dd67ef
riogu <egas.g.ribeiro@gmail.com>
2025-07-06 13:07:12 +0100

    improved main

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,17 @@ using i32 = int;
 // TODO: write a script to run all the tests for the compiler (up to current stage)
 
 auto main(int argc, char* argv[]) -> i32 {
+    Lexer lexer {};
+
     std::string test;
 
-    test = (argc > 1) ? argv[1] : "src/tests/testfile.c";
+    // test = (argc > 1) ? argv[1] : "src/tests/testfile.c";
+    // auto [tokens, file] = lexer.tokenize_file(test);
+
     test = "gamer = 69420; gamer = 1231 + 21312 * 3213 / (1231230 + 2130 + 10)";
+    auto [tokens, file] = lexer.tokenize_string("test01", test);
 
     std::print("compiling file: {}...\n", test);
-
-    Lexer lexer {};
-    // auto [tokens, file] = lexer.tokenize_file(test);
-    auto [tokens, file] = lexer.tokenize_string("test01", test);
 
     for (auto& tkn : tokens) std::print("{}\t-> {}\n", tkn.to_str(), tkn.type_name());
 
