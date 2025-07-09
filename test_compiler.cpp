f9ba99e40e0b97ea96e78c268863ead26a960851
riogu <egas.g.ribeiro@gmail.com>
2025-07-09 23:39:57 +0100

    added small changes to tests

--- test_compiler.cpp
+++ test_compiler.cpp
@@ -1,19 +1,19 @@
-#include <cstdlib>
 #include <format>
-#include <string>
-#include <vector>
 
-int main() {
+// clang-format off
+
+#define t std::tuple
+
+auto main() -> int {
+    // teststring + expected output (only for arithmetic for now)
     constexpr std::array tests {
-        "var = 69 + 21 + (3 + 3 = 2) * (-2 - 3 / ~3) + 3 + 30;\nepic = 213;",
-        // FIXME:               ^ fix this in the analysis step
-        " 123123 + ~213213* 3123;",
-        "gamer = 69420; gamer = ~1231 + 21312 * 3213 / (1231230 + 2130 + 2 * 3 - 45 + 3123 + 10);\n",
-        "var = 69 + (3  + 2 = 3);",
-        "-(-(-(-(-(-(-(-(-(1)))))))));",
-        "(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-1))))))))))))))))))))))))))))))))))));",
+        t("var = 69 + 21 + (3 + 3 = 2) * (-2 - 3 / ~3) + 3 + 30;\nepic = 213;", 0),
+        t(" 123123 + ~213213* 3123;", 0),
+        t("gamer = 69420;gamer = ~1231 + 21312 * 3213 / (1231230 + 2130 + 2 * 3 - 45 + 3123 + 10);\n",0),
+        t("var = 69 + (3  + 2 = 3);", 0),
+        t("-(-(-(-(-(-(-(-(-(1)))))))));", 0),
+        t("(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-1))))))))))))))))))))))))))))))))))));",0),
+        t("1 + 3 * 2 - 2 / 3;", 0),
     };
-
-    for (const auto& test : tests)
-        std::system(std::format("./build/c-compiler \"{}\"", test).c_str());
+    for (const auto& [test, expected] : tests) std::system(std::format("./build/c-compiler \"{}\"", test).c_str());
 }
