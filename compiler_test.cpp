3adc1f5c31bdf06e38f969a8905278a9a79c89d4
riogu <egas.g.ribeiro@gmail.com>
2025-07-19 02:59:16 +0100

    improved tests
    small fix

--- compiler_test.cpp
+++ compiler_test.cpp
@@ -1,63 +1,59 @@
+#include <cstdlib>
 #include <format>
-#include <memory>
 #include <print>
 #include <string>
 
 // clang-format off
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
+std::pair<std::string, int> exec(const char* cmd) {
+   char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
     }
-    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
+    int status = pclose(pipe);
+    return {result, status};
 }
 
 #define t std::pair
-
+constexpr std::string fail = "fail";
+constexpr std::string pass = "pass";
 auto main() -> int {
     // teststring + expected output (only for arithmetic for now)
     constexpr std::array tests {
-        // t("auto gamer = 123123;", ""),
-        // t("var = 69 + 21 + (3 + 3 = 2) * (-2 - 3 / ~3) + 3 + 30;\nepic = 213;", ""),
-        // t("123123 + ~213213* 3123;", "69"),
-        // t("gamer = 69420;gamer = ~1231 + 21312 * 3213 / (1231230 + 2130 + 2 * 3 - 45 + 3123 + 10);","0"),
-        // t("-(-(-(-(-(-(-(-(-(1)))))))));", "0"),
-        // t("var = 69 + (3  + 2 = 3);", ""),
-        // t("(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-1))))))))))))))))))))))))))))))))))));","0"),
-        // t("wow = 1 + epic - 3 * (2  + 69 - 2) / 3;", "0"),
-        // t("x = 3 + 3 * 123 - (321 + 213); x;", "0"),
-        // t("let var: int = (-1) * 3- - 8 * - 1 +2;", "69")
+        t("let var = 69 + 21 + (3 + 3 = 2) * (-2 - 3 / ~3) + 3 + 30;\nlet epic = 213;", "fail"),
+        t("123123 + ~213213* 3123;", "pass"),
+        t("let gamer = 69420;let gamer = ~1231 + 21312 * 3213 / (1231230 + 2130 + 2 * 3 - 45 + 3123 + 10);","pass"),
+        t("(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-1))))))))))))))))))))))))))))))))))));","pass"),
+        t("let var: int = (-1) * 3- - 8 * - 1 +2;", "pass"),
         t("let var: i64 = {69213 * 2 , {2, {323213, 123123}}};\n"
           "fn func_name(a: i32, b: f64) -> const i32* {}\n"
-          "fn another_f() -> const int****;\n"
+          "fn another_f() -> const i32****;\n"
           "var = {1232};\n"
+          "let var;"
           "let var: i32 = 1232;"
-          ,"")
+          ,"pass")
     };
     std::print("{}","\n------------------------------------------------\n");
     for (const auto& [test, expected] : tests) {
-        auto output = exec(std::format("./build/fumo-compiler \"{}\"", test).c_str());
-        if (output != expected) {
-            std::print("> \033[38;2;235;67;54m❌FAILED\033[0m:\n"
-                       "{}\n"
-                       "\nEXPECTED:\n"
-                       "{}\n"
-                       "\nGOT:\n"
-                       "{}"
-                       "\n------------------------------------------------\n"
-                       ,test, expected, output);
-        }
-        else {
+        auto [output, status] = exec(std::format("./build/fumo-compiler \"{}\"", test).c_str());
+        if ((expected == fail && WEXITSTATUS(status)) 
+         || (expected == pass && !WEXITSTATUS(status))) {
             std::print("> \033[38;2;88;154;143m✓ OK\033[0m:\n"
                        "{}"
                        "\n------------------------------------------------\n"
-                       , test);
+                       ,test);
+        } else {
+            std::print("> \033[38;2;235;67;54m❌FAILED\033[0m:\n"
+                       "{}\n"
+                       "\n------------------------------------------------\n"
+                       ,test);
         }
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
+#include "codegen/llvm_codegen.hpp"
 #include "lexer/lexer.hpp"
 #include "parser/parser.hpp"
-#include "codegen/llvm_codegen.hpp"
-#include <print>
 
 // TODO: write a script to run all the tests for the compiler (up to current stage)
 auto main(int argc, char* argv[]) -> int {
     std::string test;
-    
+
     if (argc > 1) test = argv[1];
     Lexer lexer {};
-    // auto [tokens, file] = lexer.tokenize_file(test);
     auto [tokens, file] = lexer.tokenize_string("test01", test);
     // for(auto& token: tokens) std::print("{} and ", token.to_str());
 
@@ -17,19 +15,16 @@ auto main(int argc, char* argv[]) -> int {
     parser.file_stream << file.contents;
 
     const auto AST = parser.parse_tokens(tokens);
-    for (auto& node : AST) std::print("node found:\n  {}\n", node->to_str());
+    for (auto& node : AST) std::cerr << std::format("node found:\n  {}\n", node->to_str());
 
     // Codegen codegen {};
     // codegen.codegen(AST);
 
-
     // Current task list:
-    // - add functions 
+    // - add functions
     // - make symbol table. each scope has its own symbols.
     //   maybe we can provide errors in the parser itself for missmatching symbols (like redefinitions or lack of declaration)
-    //   > the type solving should be deffered for later
+    //   > the type solving should be deferred for later
     // 2-make small sample llvm project to test the library
     // 3-add llvm to my project starting from smaller parts
-
-
 }
